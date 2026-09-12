a45b6ed105375b7752e9f4761243a9d237c62a50
tetraxion <lutfid955@gmail.com>
2026-09-12 12:43:18 +0700

    Add database seeder for KategoriItems and MasterItems with initial data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\KategoriItem;
+use App\Models\MasterItem;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $kategoriObat = KategoriItem::firstOrCreate(
+            ['kode' => 'KAT-0001'],
+            ['nama' => 'Obat-obatan']
+        );
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $kategoriAlkes = KategoriItem::firstOrCreate(
+            ['kode' => 'KAT-0002'],
+            ['nama' => 'Alat Kesehatan']
+        );
+
+        $kategoriMatkes = KategoriItem::firstOrCreate(
+            ['kode' => 'KAT-0003'],
+            ['nama' => 'Material Kesehatan']
+        );
+
+        $item1 = MasterItem::firstOrCreate(
+            ['kode' => '00001'],
+            [
+                'nama' => 'Paracetamol 500mg Tab',
+                'harga_beli' => 12000,
+                'laba' => 15,
+                'supplier' => 'Tokopaedi',
+                'jenis' => 'Obat',
+            ]
+        );
+
+        $item2 = MasterItem::firstOrCreate(
+            ['kode' => '00002'],
+            [
+                'nama' => 'Tensimeter Digital',
+                'harga_beli' => 250000,
+                'laba' => 20,
+                'supplier' => 'TokoBagas',
+                'jenis' => 'Alkes',
+            ]
+        );
+
+        $item1->kategoriItems()->syncWithoutDetaching([$kategoriObat->id]);
+        $item2->kategoriItems()->syncWithoutDetaching([$kategoriAlkes->id, $kategoriMatkes->id]);
     }
 }
